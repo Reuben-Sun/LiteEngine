99f216572644e7ad02aec10014a56805c12a1ce8
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2024-01-10 10:26:23 +0800

    vertex

---
+++ Engine/Geometry/Vertex.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Marco.h"
+#include "glm/glm.hpp"
+#include "vulkan/vulkan.h"
+
+namespace ToolEngine
+{
+	struct Vertex
+	{
+		glm::vec3 pos;
+		glm::vec3 color;
+		glm::vec2 texture_coord;
+
+		static VkVertexInputBindingDescription getBindingDescription()
+		{
+			VkVertexInputBindingDescription binding_description{};
+			binding_description.binding = 0;
+			binding_description.stride = sizeof(Vertex);
+			binding_description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+			return binding_description;
+		}
+
+		static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions()
+		{
+			std::vector<VkVertexInputAttributeDescription> attribute_descriptions(3);
+			// vertex object position
+			attribute_descriptions[0].binding = 0;
+			attribute_descriptions[0].location = 0;
+			attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attribute_descriptions[0].offset = offsetof(Vertex, pos);
+			// proper color
+			attribute_descriptions[1].binding = 0;
+			attribute_descriptions[1].location = 1;
+			attribute_descriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attribute_descriptions[1].offset = offsetof(Vertex, color);
+			// uv0
+			attribute_descriptions[2].binding = 0;
+			attribute_descriptions[2].location = 2;
+			attribute_descriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+			attribute_descriptions[2].offset = offsetof(Vertex, texture_coord);
+
+			return attribute_descriptions;
+		}
+	};
+}
