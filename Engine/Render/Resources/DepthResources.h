88486dc53c25440341676706e0a5fc9bc58e3316
Reuben-Sun <2824016820@qq.com>
2024-01-20 23:23:48 +0800

    move depth file

--- Engine/Render/Resources/DepthResources.h
+++ Engine/Render/Resources/DepthResources.h
@@ -18,17 +18,4 @@ namespace ToolEngine
 		std::unique_ptr<RHIImage> m_image;
 		RHIDevice& m_device;
 	};
-
-	inline DepthResources::DepthResources(RHIDevice& device, uint32_t width, uint32_t height) : m_device(device)
-	{
-		VkExtent2D extent = { width, height };
-		m_image = std::make_unique<RHIImage>(m_device, extent, m_device.getDepthFormatDetail(),
-			VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, 
-			VK_IMAGE_ASPECT_DEPTH_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-		m_image->transitionImageLayout(VK_IMAGE_LAYOUT_UNDEFINED, 
-			VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL, VK_IMAGE_ASPECT_DEPTH_BIT);
-	}
-	inline DepthResources::~DepthResources()
-	{
-	}
 }
