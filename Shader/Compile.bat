c4765c2e4bcf51ceee43c036599f9f3b2093bf09
Reuben-PC <2824016820@qq.com>
2024-01-25 22:45:33 +0800

    blit shader

--- Shader/Compile.bat
+++ Shader/Compile.bat
@@ -6,4 +6,6 @@ set TARGET_DIR=SPV
 glslangValidator -V -S vert %SHADER_DIR%/Unlit.vert -o %TARGET_DIR%/Unlit_vert.spv
 glslangValidator -V -S frag %SHADER_DIR%/Unlit.frag -o %TARGET_DIR%/Unlit_frag.spv
 
+glslangValidator -V -S vert %SHADER_DIR%/Blit.vert -o %TARGET_DIR%/Blit_vert.spv
+glslangValidator -V -S frag %SHADER_DIR%/Blit.frag -o %TARGET_DIR%/Blit_frag.spv
 pause
