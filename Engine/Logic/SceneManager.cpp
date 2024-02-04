f858cb2e09612b60296a5ec5fced3c84859939f1
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2024-02-04 16:28:21 +0800

    scene

--- Engine/Logic/SceneManager.cpp
+++ Engine/Logic/SceneManager.cpp
@@ -2,14 +2,49 @@
 
 namespace ToolEngine
 {
-	SceneManager::SceneManager()
+	SceneManager::SceneManager(const std::string& scene_json): m_current_scene(scene_json)
 	{
-		auto j = Path::getInstance().readJson("F:\\go.json");
-		GameObject go = GameObject::deserialize(j);
-		m_game_objects.push_back(go);
+		loadScene(scene_json);
 	}
 	SceneManager::~SceneManager()
 	{
 		m_game_objects.clear();
+	}
+	void SceneManager::loadScene(const std::string& scene_json)
+	{
+		if (scene_json != m_current_scene)
+		{
+			m_current_scene = scene_json;
+			std::string file_path = Path::getInstance().getAssetPath() + scene_json;
+			auto j = Path::getInstance().readJson(file_path);
+			deserialize(j);
+		}
+	}
+	void SceneManager::saveScene(const std::string& scene_json)
+	{
+		std::string file_path = Path::getInstance().getAssetPath() + scene_json;
+		auto j = serialize();
+		Path::getInstance().saveJson(file_path, j);
+	}
+	nlohmann::json SceneManager::serialize() const
+	{
+		nlohmann::json scene;
+		std::vector<nlohmann::json> game_objects;
+		for (const auto& go : m_game_objects)
+		{
+			game_objects.push_back(go.serialize());
+		}
+		scene["game_objects"] = game_objects;
+
+		return scene;
+	}
+	void SceneManager::deserialize(const nlohmann::json& j)
+	{
+		m_game_objects.clear();
+		auto game_objects = j.at("game_objects").get<std::vector<nlohmann::json>>();
+		for (const auto& go : game_objects)
+		{
+			m_game_objects.push_back(GameObject::deserialize(go));
+		}
 	}
 }
--- Engine/Logic/SceneManager.h
+++ Engine/Logic/SceneManager.h
@@ -9,9 +9,15 @@ namespace ToolEngine
 	class SceneManager final
 	{
 	public:
-		SceneManager();
+		SceneManager(const std::string& scene_json = "Scene\\Default.scene");
 		~SceneManager();
+
+		void loadScene(const std::string& scene_json);
+		void saveScene(const std::string& scene_json);
 	private:
+		std::string m_current_scene;
 		std::vector<GameObject> m_game_objects;
+		nlohmann::json serialize() const;
+		void deserialize(const nlohmann::json& j);
 	};
 }
