28427d20e53c4bfbb4fb4b6fd9f4d3f86e7c0303
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2024-02-20 14:20:55 +0800

    light struct

--- Engine/Geometry/Light.h
+++ Engine/Geometry/Light.h
@@ -1,8 +1,24 @@
 #pragma once
 
 #include "Marco.h"
+#include <glm/glm.hpp>
 
 namespace ToolEngine
 {
+	struct Light 
+	{
+		glm::vec3 position;
+		glm::vec3 color;
+		float intensity;
+	};
 
+	struct DirectionalLight : public Light
+	{
+		glm::vec3 direction;
+	};
+
+	struct LightUBO
+	{
+		DirectionalLight directional;
+	};
 }
