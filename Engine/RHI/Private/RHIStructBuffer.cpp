04f6142c4af23a11337b9b5af84e10dde0495816
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2024-02-27 18:38:13 +0800

    struct buffer

--- Engine/RHI/Private/RHIStructBuffer.cpp
+++ Engine/RHI/Private/RHIStructBuffer.cpp
@@ -2,5 +2,29 @@
 
 namespace ToolEngine
 {
+    RHIStructBuffer::RHIStructBuffer(RHIDevice& device, size_t buffer_size): m_device(device), m_buffer_size(buffer_size)
+    {
+        m_device.createBuffer(m_buffer_size, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_buffer, m_memory);
+        vkMapMemory(m_device.getLogicalDevice(), m_memory, 0, m_buffer_size, 0, &m_buffer_mapped);
 
+        m_descriptor.buffer = m_buffer;
+        m_descriptor.offset = 0;
+        m_descriptor.range = m_buffer_size;
+    }
+    RHIStructBuffer::~RHIStructBuffer()
+    {
+        if (m_buffer != VK_NULL_HANDLE)
+        {
+            vkDestroyBuffer(m_device.getLogicalDevice(), m_buffer, nullptr);
+        }
+        if (m_memory != VK_NULL_HANDLE)
+        {
+            vkFreeMemory(m_device.getLogicalDevice(), m_memory, nullptr);
+        }
+    }
+    void RHIStructBuffer::updateBuffer(void* ubo)
+    {
+        memcpy(m_buffer_mapped, ubo, m_buffer_size);
+    }
 }
--- Engine/RHI/Public/RHIStructBuffer.h
+++ Engine/RHI/Public/RHIStructBuffer.h
@@ -1,8 +1,26 @@
 #pragma once
 
 #include "Marco.h"
+#include <vulkan/vulkan.h>
+#include "RHIDevice.h"
 
 namespace ToolEngine
 {
+	class RHIStructBuffer
+	{
+	public:
+		RHIStructBuffer(RHIDevice& device, size_t buffer_size);
+		~RHIStructBuffer();
 
+		void updateBuffer(void* ubo);
+		VkBuffer getHandle() const { return m_buffer; }
+		VkDescriptorBufferInfo m_descriptor;
+	private:
+		RHIDevice& m_device;
+
+		size_t m_buffer_size;
+		VkBuffer m_buffer;
+		VkDeviceMemory m_memory;
+		void* m_buffer_mapped;
+	};
 }
