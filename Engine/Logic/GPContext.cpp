7e61335cc60308b935e698825a56e4c032bb4065
Reuben-Sun <2824016820@qq.com>
2024-03-03 17:57:39 +0800

    delete component.cpp

--- Engine/Logic/GPContext.cpp
+++ Engine/Logic/GPContext.cpp
@@ -5,6 +5,7 @@ namespace ToolEngine
 {
     GPContext::GPContext()
     {
+        // scene manager need first init
         m_scene_manager = std::make_unique<SceneManager>(m_logic_scene);
         m_fps_camera = std::make_unique<FPSCamera>(m_scene);
         //m_physics_manager = std::make_unique<PhysicsManager>(m_scene);
@@ -15,8 +16,9 @@ namespace ToolEngine
     void GPContext::tick()
     {
         OPTICK_EVENT();
-        m_scene_manager->tick(m_scene);
         m_fps_camera->tick();
         //m_physics_manager->tick();
+        // scene manager need last tick
+        m_scene_manager->tick(m_scene);
     }
 }
