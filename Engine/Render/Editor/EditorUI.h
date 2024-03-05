fc387b0bae307d5428d22add3f87a0b8d82d327b
Reuben-PC <2824016820@qq.com>
2024-03-05 23:09:19 +0800

    分离imgui

--- Engine/Render/Editor/EditorUI.h
+++ Engine/Render/Editor/EditorUI.h
@@ -1,8 +1,44 @@
 #pragma once
 
 #include "Marco.h"
+#include <imgui.h>
+#include <backends/imgui_impl_vulkan.h>
+#include <backends/imgui_impl_glfw.h>
+#include "RHI/Public/RHIContext.h"
+#include "Render/Pass/UIPass.h"
+#include "RHI/Public/RHIFrameBuffer.h"
+#include "RHI/Public/RHICommandBuffer.h"
+#include "RHI/Public/RHIDescriptorSet.h"
+#include "RHI/Public/RHITextureImage.h"
 
 namespace ToolEngine
 {
+	class EditorUI
+	{
+	public:
+		EditorUI(RHIContext& rhi_context);
+		~EditorUI();
 
+		void record(RHICommandBuffer& cmd, uint32_t frame_index);
+	private:
+		RHIContext& m_rhi_context;
+
+		std::unique_ptr<UIPass> m_ui_pass;
+		std::vector<std::unique_ptr<RHIFrameBuffer>> m_ui_frame_buffers;
+		std::unique_ptr<RHIDescriptorSetLayout> m_texture_descriptor_set_layout;
+		std::unordered_map<std::string, std::unique_ptr<RHITextureImage>> m_texture_name_to_image;
+		std::unordered_map<std::string, std::unique_ptr<RHIDescriptorSet>> m_texture_name_to_descriptor_set;
+		std::string m_current_path;
+		float m_browser_button_size = 100.0f;
+		float m_browser_button_spacing = 10.0f;
+
+		void initImGui();
+		void setImGuiStyle();
+		void drawMainMenuBar();
+		void drawHierarchy();
+		void drawScene();
+		void drawBrowser();
+		void drawDetail();
+		std::string selectIcon(const std::string& file_name);
+	};
 }
--- Engine/Render/RenderContext.h
+++ Engine/Render/RenderContext.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "Marco.h"
-#include <imgui.h>
-#include <backends/imgui_impl_vulkan.h>
-#include <backends/imgui_impl_glfw.h>
+
 #include "RHI/Public/RHIContext.h"
 #include "Render/Editor/EditorUI.h"
 #include "Render/Runtime/Render.h"
@@ -13,6 +11,7 @@
 #include "RHI/Public/RHIDescriptorSet.h"
 #include "RHI/Public/RHITextureImage.h"
 #include "Geometry/RenderScene.h"
+#include "Render/Editor/EditorUI.h"
 
 namespace ToolEngine
 {
@@ -26,9 +25,9 @@ namespace ToolEngine
 	private:
 		RHIContext& m_rhi_context;
 		
+		std::unique_ptr<EditorUI> m_editor_ui;
 		std::unique_ptr<RHICommandBuffer> m_command_buffer;
-		std::unique_ptr<UIPass> m_ui_pass;
-		std::vector<std::unique_ptr<RHIFrameBuffer>> m_ui_frame_buffers;
+		
 		std::vector<std::unique_ptr<Fence>> m_in_flight_fences;
 		std::vector<std::unique_ptr<Semaphore>> m_image_available_semaphores;
 		std::vector<std::unique_ptr<Semaphore>> m_render_finished_semaphores;
@@ -40,20 +39,7 @@ namespace ToolEngine
 		bool prepareFrame(uint32_t& image_index);
 		void submitFrame(uint32_t image_index);
 
-		std::unique_ptr<RHIDescriptorSetLayout> m_texture_descriptor_set_layout;
-		std::unordered_map<std::string, std::unique_ptr<RHITextureImage>> m_texture_name_to_image;
-		std::unordered_map<std::string, std::unique_ptr<RHIDescriptorSet>> m_texture_name_to_descriptor_set;
-		std::string m_current_path;
-		float m_browser_button_size = 100.0f;
-		float m_browser_button_spacing = 10.0f;
-		void initImGui();
-		void setImGuiStyle();
-		void drawUI();
-		void drawMainMenuBar();
-		void drawHierarchy();
-		void drawScene();
-		void drawBrowser();
-		void drawDetail();
-		std::string selectIcon(const std::string& file_name);
+		
+		
 	};
 }
