6354a216e0e3a35fcf121e8be880c1a160a35f2a
Reuben-PC <2824016820@qq.com>
2024-03-10 23:31:07 +0800

    rename state

--- Engine/Render/Pipeline/SkyboxPipeline.cpp
+++ Engine/Render/Pipeline/SkyboxPipeline.cpp
@@ -66,21 +66,21 @@ namespace ToolEngine
 		pipeline_layout_info.pPushConstantRanges = push_constant_ranges.data();
 		m_pipeline_layout = std::make_unique<RHIPipelineLayout>(m_device, pipeline_layout_info);
 
-		RHIPipelineState m_state;
-		m_state.vertex_shader_stage = vert_shader_stage_info;
-		m_state.fragment_shader_stage = frag_shader_stage_info;
-		m_state.pipeline_layout = m_pipeline_layout->getHandle();
-		m_state.render_pass = m_render_pass;
-		m_state.vertex_input_state = vertex_input_state;
-		m_state.input_assembly_state = input_assembly_state;
-		m_state.viewport_state = viewport_state;
-		m_state.rasterization_state = rasterization_state;
-		m_state.multisample_state = multi_sample_state;
-		m_state.depth_stencil_state = depth_stencil_state;
-		m_state.color_blend_state = color_blending;
-		m_state.dynamic_state = dynamicState;
-		m_state.subpass_index = 0;
+		RHIPipelineState state;
+		state.vertex_shader_stage = vert_shader_stage_info;
+		state.fragment_shader_stage = frag_shader_stage_info;
+		state.pipeline_layout = m_pipeline_layout->getHandle();
+		state.render_pass = m_render_pass;
+		state.vertex_input_state = vertex_input_state;
+		state.input_assembly_state = input_assembly_state;
+		state.viewport_state = viewport_state;
+		state.rasterization_state = rasterization_state;
+		state.multisample_state = multi_sample_state;
+		state.depth_stencil_state = depth_stencil_state;
+		state.color_blend_state = color_blending;
+		state.dynamic_state = dynamicState;
+		state.subpass_index = 0;
 		m_pipeline = std::make_unique<RHIPipeline>(m_device);
-		m_pipeline->createPipeline(m_state);
+		m_pipeline->createPipeline(state);
     }
 }
