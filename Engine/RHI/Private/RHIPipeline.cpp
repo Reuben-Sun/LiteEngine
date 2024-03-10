d636795279596f2c0a3a0b995895c1227e277abb
Reuben-PC <2824016820@qq.com>
2024-03-10 23:26:06 +0800

    优化pipeline创建

--- Engine/RHI/Private/RHIPipeline.cpp
+++ Engine/RHI/Private/RHIPipeline.cpp
@@ -20,19 +20,27 @@ namespace ToolEngine
             m_pipeline_state.m_vertex_shader_stage.getHandle(), 
             m_pipeline_state.m_fragment_shader_stage.getHandle()
         };
+        VkPipelineVertexInputStateCreateInfo vertex_input_state = m_pipeline_state.m_vertex_input_state.getHandle();
+        VkPipelineInputAssemblyStateCreateInfo input_assembly_state = m_pipeline_state.m_input_assembly_state.getHandle();
+        VkPipelineViewportStateCreateInfo viewport_state = m_pipeline_state.m_viewport_state.getHandle();
+        VkPipelineRasterizationStateCreateInfo rasterization_state = m_pipeline_state.m_rasterization_state.getHandle();
+        VkPipelineMultisampleStateCreateInfo multisample_state = m_pipeline_state.m_multisample_state.getHandle();
+        VkPipelineDepthStencilStateCreateInfo depth_stencil_state = m_pipeline_state.m_depth_stencil_state.getHandle();
+        VkPipelineColorBlendStateCreateInfo color_blend_state = m_pipeline_state.m_color_blend_state.getHandle();
+        VkPipelineDynamicStateCreateInfo dynamic_state = m_pipeline_state.m_dynamic_state.getHandle();
 
         VkGraphicsPipelineCreateInfo pipeline_info{};
         pipeline_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
         pipeline_info.stageCount = shader_stages.size();
         pipeline_info.pStages = shader_stages.data();
-        pipeline_info.pVertexInputState = &m_pipeline_state.m_vertex_input_state;
-        pipeline_info.pInputAssemblyState = &m_pipeline_state.m_input_assembly_state;
-        pipeline_info.pViewportState = &m_pipeline_state.m_viewport_state;
-        pipeline_info.pRasterizationState = &m_pipeline_state.m_rasterization_state;
-        pipeline_info.pMultisampleState = &m_pipeline_state.m_multisample_state;
-        pipeline_info.pDepthStencilState = &m_pipeline_state.m_depth_stencil_state;
-        pipeline_info.pColorBlendState = &m_pipeline_state.m_color_blend_state;
-        pipeline_info.pDynamicState = &m_pipeline_state.m_dynamic_state;
+        pipeline_info.pVertexInputState = &vertex_input_state;
+        pipeline_info.pInputAssemblyState = &input_assembly_state;
+        pipeline_info.pViewportState = &viewport_state;
+        pipeline_info.pRasterizationState = &rasterization_state;
+        pipeline_info.pMultisampleState = &multisample_state;
+        pipeline_info.pDepthStencilState = &depth_stencil_state;
+        pipeline_info.pColorBlendState = &color_blend_state;
+        pipeline_info.pDynamicState = &dynamic_state;
         pipeline_info.layout = m_pipeline_state.m_pipeline_layout;
         pipeline_info.renderPass = m_pipeline_state.m_render_pass;
         pipeline_info.subpass = m_pipeline_state.m_subpass_index;
