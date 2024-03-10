8c88262fc26e3d23b340ee92a6b5c2899c325a99
Reuben-PC <2824016820@qq.com>
2024-03-11 00:26:11 +0800

    render outline init

--- Engine/Render/Runtime/Feature/Outline/RenderOutline.h
+++ Engine/Render/Runtime/Feature/Outline/RenderOutline.h
@@ -1,8 +1,19 @@
 #pragma once
 
 #include "Marco.h"
+#include "RHI/Public/RHIDevice.h"
+#include "RHI/Public/RHIDescriptorPool.h"
+#include "RHI/Public/RHIVertexBuffer.h"
+#include "RHI/Public/RHIIndexBuffer.h"
+#include "RHI/Public/RHIRenderPass.h"
+#include "RHI/Public/RHICommandBuffer.h"
+#include "Render/Pipeline/SkyboxPipeline.h"
+#include "Geometry/Camera.h"
 
 namespace ToolEngine
 {
+	class RenderOutline
+	{
 
+	};
 }
